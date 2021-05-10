d1d61b93f09b5e39fab0ab118fc1691bf6272b00
Avinash Kumar <avirias@live.com>
2021-05-10 17:58:45 +0530

    location improvement, added location flow

--- app/src/main/java/com/avirias/lilith_android/MainActivity.kt
+++ app/src/main/java/com/avirias/lilith_android/MainActivity.kt
@@ -1,42 +1,65 @@
 package com.avirias.lilith_android
 
 import android.Manifest
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.avirias.lilith_android.databinding.ActivityMainBinding
-import com.eden.lilith.utils.permission
+import com.eden.lilith.utils.locationFlow
 import com.eden.lilith.utils.permissions
-import com.eden.lilith.utils.registerForResult
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val locationProvider by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
 
-    private val cameraPermission by permission {
-       if (it) takePicture.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     private val locationPermission by permissions {
-
-    }
-
-    private val takePicture by registerForResult {
-        if (it.resultCode == RESULT_OK) {
-            val bitmap = intent?.extras?.get("data") as Bitmap
+        if (it.filterValues { e -> e.not() }.isEmpty()) {
+            getLocation()
         }
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        cameraPermission.launch(Manifest.permission.CAMERA)
+        binding.button.setOnClickListener {
+            locationPermission.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            )
+        }
 
+    }
+
+    private fun getLocation() {
+
+        locationProvider.locationFlow()
+            .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .onEach {
+                Log.d(TAG, "onCreate: location => lat: ${it.latitude}, long: ${it.longitude}")
+            }.launchIn(lifecycleScope)
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 
 }
--- lilith/src/main/java/com/eden/lilith/utils/Location.kt
+++ lilith/src/main/java/com/eden/lilith/utils/Location.kt
@@ -3,8 +3,16 @@ package com.eden.lilith.utils
 import android.Manifest
 import android.location.Location
 import android.location.LocationManager
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.annotation.RequiresPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 
 @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
 fun ComponentActivity.getLocation(
@@ -15,5 +23,35 @@ fun ComponentActivity.getLocation(
 
     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0F) {
         callback(it)
+    }
+}
+
+
+@ExperimentalCoroutinesApi
+fun FusedLocationProviderClient.locationFlow(
+    locationRequest: LocationRequest = LocationRequest.create()
+        .apply {
+            interval = 500L
+            fastestInterval = 1000L
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+) = callbackFlow<Location> {
+    val callback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            result ?: return
+            try {
+                result.locations.forEach {
+                    offer(it)
+                }
+            } catch (e: Exception) {
+            }
+        }
+    }
+    requestLocationUpdates(locationRequest, callback, Looper.getMainLooper())
+        .addOnFailureListener { e ->
+            close(e)
+        }
+    awaitClose {
+        removeLocationUpdates(callback)
     }
 }
